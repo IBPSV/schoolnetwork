73917511d1f5ecf7a653fc139dda95ce0a625f24
netoangel105 <netoangel105@gmail.com>
2015-03-23 20:46:54 -0600

    arreglo del coming soon

---
+++ LandingC/index.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0,maximun-scale=1.0, minimun-scale=1.0"> 
+<title>I.B.P.SV</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/style1.css" rel="stylesheet" type="text/css">
+<script src="js/script.js"></script>
+
+
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/stylehover.css" />
+        <link href='http://fonts.googleapis.com/css?family=Kelly+Slab' rel='stylesheet' type='text/css' />
+		<noscript>
+			<link rel="stylesheet" type="text/css" href="css/noscript.css" />
+		</noscript>
+		<script type="text/javascript" src="js/modernizr.custom.72835.js"></script>
+			<!--texto animado-->
+			<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/demo4.css" />
+        <link rel="stylesheet" type="text/css" href="css/style4.css" />
+		<script src="js/modernizr.custom.72111.js"></script>
+		<style>
+			.no-cssanimations .rw-wrapper .rw-sentence span:first-child{
+				opacity: 1;
+			}
+		</style>
+			<!--texto animado-->
+
+</head>
+
+<body>
+	<div class="tainer">
+
+		<script type="text/javascript">
+			
+			//cambiar configuración
+			
+			año=2016;
+			mes=1;
+			dia=1;
+			hora=1;
+			minuto=0;
+			segundo=0;
+			urlsitio = 'http://www.tusitio.com/index.php'; //url del sitio web final			
+
+			dateFuture = new Date(año, mes-1 , dia, hora, minuto, segundo);		
+			window.onload=function(){GetCount();}
+		
+		</script>
+		<div class="container1">
+			   <div class="wrapper">
+			
+				<div class="section">
+					<div class="normal-trigger-area">
+						<a href="#" id="circle" class="ec-circle">
+							<h3>COMING SOON</h3>
+						</a>
+					</div>
+					
+				</div>
+			
+            </div>
+
+        	    <!--texto animado-->
+            <div class="textoa">
+<section class="rw-wrapper">
+				<h2 class="rw-sentence">
+					<span>We design &amp; develop</span>
+					<div class="rw-words rw-words-1">
+						<span>beautiful websites</span>
+						<span>interesting apps</span>
+						<span>impactful identities</span>
+						<span>working strategies</span>
+						<span>incredible gadgets</span>
+						<span>intelligent systems</span>
+					</div>
+					<div class="rw-words rw-words-2">
+						<span>that captivate your users</span>
+						<span>that create huge revenue</span>
+						<span>that make you stand out</span>
+						<span>that will push you forward</span>
+						<span>that make your life easy</span>
+						<span>that automatize processes</span>
+					</div>
+				</h2>
+			</section>
+			<h2 class="rw-sentence-IE" ><font color="white">We design &amp; develop beautiful websites that captivate your users</font></h2>
+			</div>
+            <!--texto animado-->
+         
+       
+		<div id="contenedor">
+		
+
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.circlemouse.js"></script>
+        <script type="text/javascript">
+            $(function() {
+				
+				$('#circle').circlemouse({
+					onMouseEnter	: function( el ) {
+					
+						el.addClass('ec-circle-hover');
+					
+					},
+					onMouseLeave	: function( el ) {
+						
+						el.removeClass('ec-circle-hover');
+						
+					},
+					onClick			: function( el ) {
+						
+						alert('clicked');
+						
+					}
+				});
+				
+			});
+        </script>
+
+
+
+
+	
+			
+			<!--cambiar texto-->
+			<div id="textosuperior">
+				Proximamente.......esperalo!
+			</div>
+
+			<div id="countbox" ></div>
+
+			<!--cambiar texto-->
+			<div id="textoinferior">
+				School Network
+			</div>
+			
+		</div>
+		 </div>
+		</div>
+</body>
+</html>
